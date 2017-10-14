08a1cbc81f3d0d627e19b5f133c0de1320096396
William Wong <compulim@hotmail.com>
2017-10-13 19:31:27 -0700

    Add more tests

--- src/Stories/Controls/ButtonCard.js
+++ src/Stories/Controls/ButtonCard.js
@@ -11,6 +11,25 @@ const {
 } = Bare;
 
 export default class ButtonCard extends React.PureComponent {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleReset  = this.handleReset.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleReset(evt) {
+    evt.preventDefault();
+
+    alert('Form reset');
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+
+    alert('Form submitted');
+  }
+
   render() {
     return (
       <div { ...CARD_CSS }>
@@ -22,10 +41,26 @@ export default class ButtonCard extends React.PureComponent {
           Commodo adipisicing incididunt pariatur <Button disabled={ true } >&lt;Button disabled={ true }&gt;</Button> exercitation minim.
         </p>
         <p>
-          Nulla amet <Button href="https://bing.com/">&lt;Button href="https://bing.com/"&gt;</Button> ullamco non nisi voluptate proident est ex eu excepteur minim.
+          Nulla amet <Button href="https://bing.com/" target="_blank">&lt;Button href="https://bing.com/" target="_blank"&gt;</Button> ullamco non nisi voluptate proident est ex eu excepteur minim.
         </p>
         <p>
           Lorem dolore ut magna aliqua <Button disabled={ true } href="https://bing.com/">&lt;Button disabled={ true } href="https://bing.com/"&gt;</Button> do ea officia deserunt velit.
+        </p>
+        <form onSubmit={ this.handleSubmit }>
+          <p>
+            Sunt consequat do excepteur magna <Button type="submit">&lt;Button type="submit"&gt;</Button> ipsum id exercitation nisi cupidatat reprehenderit.
+          </p>
+        </form>
+        <p>
+          Minim non veniam cillum ea sunt sint <Button type="submit" disabled={ true }>&lt;Button type="submit" disabled={ '{ true }' }&gt;</Button> adipisicing culpa incididunt deserunt.
+        </p>
+        <form onReset={ this.handleReset }>
+          <p>
+            Non ullamco et sint qui <Button type="reset">&lt;Button type="reset"&gt;</Button> id anim esse consequat.
+          </p>
+        </form>
+        <p>
+          Ullamco consequat aute <Button type="file">&lt;Button type="file"&gt;</Button> consequat incididunt velit.
         </p>
       </div>
     );
