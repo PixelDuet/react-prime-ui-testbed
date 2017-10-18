9e4c3836e9165493c4d08810754f13a0b03759e6
William Wong <compulim@hotmail.com>
2017-10-18 04:55:44 -0700

    Check label boundary

--- src/Stories/Controls/TextBoxCard.js
+++ src/Stories/Controls/TextBoxCard.js
@@ -29,40 +29,48 @@ export default class TextBoxCard extends React.PureComponent {
     return (
       <div { ...CARD_CSS }>
         <h1>&lt;TextBox&gt;</h1>
-        <p>
-          Velit in occaecat do esse velit ullamco amet <TextBox
-            onChange   ={ this.handleChange }
-            placeholder="8-16 characters"
-            value      ={ this.state.value }
-          /> consequat voluptate id irure consequat dolore.
-        </p>
+        <label>
+          <p>
+            Velit in occaecat do esse velit ullamco amet <TextBox
+              onChange   ={ this.handleChange }
+              placeholder="8-16 characters"
+              value      ={ this.state.value }
+            /> consequat voluptate id irure consequat dolore.
+          </p>
+        </label>
         <p>
           You typed "{ this.state.value }".
         </p>
-        <p>
-          As read-only. <TextBox
-            onChange   ={ this.handleChange }
-            placeholder="8-16 characters"
-            readOnly   ={ true }
-            value      ={ this.state.value }
-          />
-        </p>
-        <p>
-          As disabled. <TextBox
-            disabled   ={ true }
-            onChange   ={ this.handleChange }
-            placeholder="8-16 characters"
-            value      ={ this.state.value }
-          />
-        </p>
-        <p>
-          As password. <TextBox
-            onChange   ={ this.handleChange }
-            placeholder="8-16 characters"
-            type       ="password"
-            value      ={ this.state.value }
-          />
-        </p>
+        <label>
+          <p>
+            As read-only. <TextBox
+              onChange   ={ this.handleChange }
+              placeholder="8-16 characters"
+              readOnly   ={ true }
+              value      ={ this.state.value }
+            />
+          </p>
+        </label>
+        <label>
+          <p>
+            As disabled. <TextBox
+              disabled   ={ true }
+              onChange   ={ this.handleChange }
+              placeholder="8-16 characters"
+              value      ={ this.state.value }
+            />
+          </p>
+        </label>
+        <label>
+          <p>
+            As password. <TextBox
+              onChange   ={ this.handleChange }
+              placeholder="8-16 characters"
+              type       ="password"
+              value      ={ this.state.value }
+            />
+          </p>
+        </label>
       </div>
     );
   }
