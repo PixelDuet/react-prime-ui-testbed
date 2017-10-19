97a79f5edbd6c3b0fd5990a5d3eda63fb746a729
William Wong <compulim@hotmail.com>
2017-10-19 01:27:44 -0700

    Label wrapping button

--- src/Stories/Controls/ButtonCard.js
+++ src/Stories/Controls/ButtonCard.js
@@ -34,34 +34,50 @@ export default class ButtonCard extends React.PureComponent {
     return (
       <div { ...CARD_CSS }>
         <h1>&lt;Button&gt;</h1>
-        <p>
-          Ea quis mollit officia sunt cillum <Button>&lt;Button&gt;</Button> esse ut.
-        </p>
-        <p>
-          Commodo adipisicing incididunt pariatur <Button disabled={ true } >&lt;Button disabled={ '{ true }' }&gt;</Button> exercitation minim.
-        </p>
-        <p>
-          Nulla amet <Button href="https://bing.com/" target="_blank">&lt;Button href="&hellip;"&gt;</Button> ullamco non nisi voluptate proident est ex eu excepteur minim.
-        </p>
-        <p>
-          Lorem dolore ut magna aliqua <Button disabled={ true } href="https://bing.com/">&lt;Button disabled={ '{ true }' } href="https://bing.com/"&gt;</Button> do ea officia deserunt velit.
-        </p>
+        <label>
+          <p>
+            Ea quis mollit officia sunt cillum <Button>&lt;Button&gt;</Button> esse ut.
+          </p>
+        </label>
+        <label>
+          <p>
+            Commodo adipisicing incididunt pariatur <Button disabled={ true } >&lt;Button disabled={ '{ true }' }&gt;</Button> exercitation minim.
+          </p>
+        </label>
+        <label>
+          <p>
+            Nulla amet <Button href="https://bing.com/" target="_blank">&lt;Button href="&hellip;"&gt;</Button> ullamco non nisi voluptate proident est ex eu excepteur minim.
+          </p>
+        </label>
+        <label>
+          <p>
+            Lorem dolore ut magna aliqua <Button disabled={ true } href="https://bing.com/">&lt;Button disabled={ '{ true }' } href="https://bing.com/"&gt;</Button> do ea officia deserunt velit.
+          </p>
+        </label>
         <form onSubmit={ this.handleSubmit }>
-          <p>
-            Sunt consequat do excepteur magna <Button type="submit">&lt;Button type="submit"&gt;</Button> ipsum id exercitation nisi cupidatat reprehenderit.
-          </p>
+          <label>
+            <p>
+              Sunt consequat do excepteur magna <Button type="submit">&lt;Button type="submit"&gt;</Button> ipsum id exercitation nisi cupidatat reprehenderit.
+            </p>
+          </label>
         </form>
-        <p>
-          Minim non veniam cillum ea sunt sint <Button type="submit" disabled={ true }>&lt;Button type="submit" disabled={ '{ true }' }&gt;</Button> adipisicing culpa incididunt deserunt.
-        </p>
+        <label>
+          <p>
+            Minim non veniam cillum ea sunt sint <Button type="submit" disabled={ true }>&lt;Button type="submit" disabled={ '{ true }' }&gt;</Button> adipisicing culpa incididunt deserunt.
+          </p>
+        </label>
         <form onReset={ this.handleReset }>
-          <p>
-            Non ullamco et sint qui <Button type="reset">&lt;Button type="reset"&gt;</Button> id anim esse consequat.
-          </p>
+          <label>
+            <p>
+              Non ullamco et sint qui <Button type="reset">&lt;Button type="reset"&gt;</Button> id anim esse consequat.
+            </p>
+          </label>
         </form>
-        <p>
-          Ullamco consequat aute <Button type="file">&lt;Button type="file"&gt;</Button> consequat incididunt velit.
-        </p>
+        <label>
+          <p>
+            Ullamco consequat aute <Button type="file">&lt;Button type="file"&gt;</Button> consequat incididunt velit.
+          </p>
+        </label>
       </div>
     );
   }
